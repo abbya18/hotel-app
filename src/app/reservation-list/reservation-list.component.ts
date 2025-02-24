7f5d574f2489d73d461b4db6c5c994ae394c804d
pentelala <abbyaarce@gmail.com>
2025-02-24 17:27:16 -0600

    Utilized a mock-API such as Mockoon to simulate CRUD operations that perform HTML calls (Get, Post, Put, Delete).

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -15,12 +15,15 @@ export class ReservationListComponent implements OnInit {
   constructor(private reservationService: ReservationService){}
 
   ngOnInit(): void {
-    this.reservations = this.reservationService.getReservations();
+    this.reservationService.getReservations().subscribe(reservations => 
+      this.reservations = reservations);
   }
 
   // Calling the service method to delete a reservation based on its id
   deleteReservation(id: string){
-    this.reservationService.deleteReservation(id);
+    this.reservationService.deleteReservation(id).subscribe(() => {
+      console.log("Delete request has been processed.")
+    });
   }
 
 }
--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,54 +1,50 @@
 import { Injectable } from '@angular/core';
 import { Reservation } from '../models/reservation';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 // Meth
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
+
+
+  private apiUrl ="http://localhost:3000"   // api URL to mockoon
   private reservations: Reservation[] = [];
 
-  constructor(){
-    let savedReservations = localStorage.getItem("reservations");
-    this.reservations = savedReservations? JSON.parse(savedReservations) : [];
+  constructor(private http: HttpClient){
+
   }
+
 
   // CRUD Operations that we want to provide
   // This is using an array and not like a database.
 
-
-
-  // Equivalent of GetAll
-  getReservations(): Reservation[]{
-    return this.reservations;
+  // Equivalent of GetAllAsync (Observable makes it async)
+  getReservations(): Observable<Reservation[]>{
+    return this.http.get<Reservation[]>(this.apiUrl + "/reservations");
   }
 
   // Equivalent of getting a specific item
-  getReservation(id: string): Reservation | undefined {
-    return this.reservations.find(res => res.id === id);
+  getReservation(id: string): Observable<Reservation> {
+    return this.http.get<Reservation>(this.apiUrl + "/reservation/" + id);
   }
 
   // Equivalent of Post
-  addReservation(reservation: Reservation): void {
-    reservation.id = Date.now.toString();
+  addReservation(reservation: Reservation): Observable<void> {
+    return this.http.post<void>(this.apiUrl + "/reservation", reservation);
 
-    this.reservations.push(reservation);
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   // Equivalent of Delete
-  deleteReservation(id: string): void {
-    let index = this.reservations.findIndex(res => res.id === id);
-    this.reservations.splice(index, 1)
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
+  deleteReservation(id: string): Observable<void> {
+    return this.http.delete<void>(this.apiUrl + "/reservation/" + id);
   }
 
   // Equivalent of Put
-  updateReservation(id: string, updatedReservation: Reservation): void {
-    let index = this.reservations.findIndex(res => res.id === id)
-    this.reservations[index] = updatedReservation;
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
+  updateReservation(id: string, updatedReservation: Reservation): Observable<void> {
+    return this.http.put<void>(this.apiUrl + "/reservation/" + id, updatedReservation);
   }
 
 }
